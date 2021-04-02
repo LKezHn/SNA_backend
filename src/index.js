066c676963762ceec28ff2cc659082b3f11d5575
Luis <mluise2406@gmail.com>
2021-04-01 20:48:03 -0600

    ProcFile added

--- src/index.js
+++ src/index.js
@@ -10,10 +10,9 @@ require('./database.js');
 app.use(express.json());
 app.use('/api/v1',router);
 
-
 function init(){
-  app.listen(4000);
-  console.log('Connected on port 4000')
+  app.listen(process.env.PORT || 4000)
+  console.log(`Connected on port ${process.env.PORT}`)
 }
 
 init();
